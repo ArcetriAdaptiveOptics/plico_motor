60ab282193ef8626a0190aff7b51c0cb8a41f535
Lorenzo Busoni <lbusoni@gmail.com>
2021-12-23 18:22:03 +0100

    python-package workflow setup

--- plico_motor/client/abstract_motor_client.py
+++ plico_motor/client/abstract_motor_client.py
@@ -2,7 +2,6 @@ import abc
 from six import with_metaclass
 from plico.utils.decorator import returnsNone, returns, returnsForExample
 from plico_motor.types.motor_status import MotorStatus
-from plico_motor.client.snapshot_entry import SnapshotEntry
 
 
 class AbstractMotorClient(with_metaclass(abc.ABCMeta, object)):
@@ -37,35 +36,34 @@ class AbstractMotorClient(with_metaclass(abc.ABCMeta, object)):
     def status(self):
         assert False
 
-
-def _proposed_interface():
-    x = plico_motor.client('ip', axis=0, port=port)
-    x.home()
-    x.home(limit=x.NEG)
-    curr_pos = x.position()
-    x.move_by(-23)
-    x.move_to(123)
-
-    # status. A given field can be MotorClient.NOT_AVAILABLE if the motor is
-    # not providing the info.
-    status = x.status()
-    assert status.has_been_homed
-    steps_per_m = status.steps_per_m
-    max_speed = status.max_speed
-    curr_pos = status.position
-    is_moving = status.is_moving
-    should_be_in = status.last_commanded_position
-
-    # snapshot is like status in a dictionary form ready for FITS headers
-    dicto = x.snapshot('MYAXIS1')
-    assert dicto['MYAXIS1.POS'] == 123
-    # and so on for each status' fields
-
-    # boh... nice to have. Need to modify config file on server side.
-    x.move_to('A')
-    x.save_current_position_as('A')
-    x.list_positions()
-
-    # boh... Is this needed in the client? Static conf should be enough.
-    x.set_max_speed(100)
+# def _proposed_interface():
+#     x = plico_motor.client('ip', axis=0, port=port)
+#     x.home()
+#     x.home(limit=x.NEG)
+#     curr_pos = x.position()
+#     x.move_by(-23)
+#     x.move_to(123)
+#
+#     # status. A given field can be MotorClient.NOT_AVAILABLE if the motor is
+#     # not providing the info.
+#     status = x.status()
+#     assert status.has_been_homed
+#     steps_per_m = status.steps_per_m
+#     max_speed = status.max_speed
+#     curr_pos = status.position
+#     is_moving = status.is_moving
+#     should_be_in = status.last_commanded_position
+#
+#     # snapshot is like status in a dictionary form ready for FITS headers
+#     dicto = x.snapshot('MYAXIS1')
+#     assert dicto['MYAXIS1.POS'] == 123
+#     # and so on for each status' fields
+#
+#     # boh... nice to have. Need to modify config file on server side.
+#     x.move_to('A')
+#     x.save_current_position_as('A')
+#     x.list_positions()
+#
+#     # boh... Is this needed in the client? Static conf should be enough.
+#     x.set_max_speed(100)
 
