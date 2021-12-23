063466b82ed0e0341ebe76980be32c7ca9ffbbc7
Alfio Puglisi <alfio.puglisi@inaf.it>
2021-12-23 13:02:03 +0100

    Motor GUI now working

--- plico_motor/gui/motor_control_gui.py
+++ plico_motor/gui/motor_control_gui.py
@@ -7,27 +7,42 @@ from guietta import Gui, _
 class Runner(object):
 
     def __init__(self):
-        self._motor = plico_motor.motor(host, port)
+        pass
 
     def _setUp(self, argv):
 
-        def moveby(gui):
-            self._motor.moveby(int(gui.nsteps))
+        host, port = argv
+        self._motor = plico_motor.motor(host, int(port))
+        print(dir(self._motor))
 
-        def getpos(gui):
+        def moveby(gui):
+            nsteps = int(gui.nstepsby)
+            self._motor.move_by(nsteps)
+
+        def moveto(gui):
+            nsteps = int(gui.nstepsto)
+            self._motor.move_to(nsteps)
+
+        def getstatus(gui):
             try:
-                gui.pos = self._motor.pos()
-            except pico.PicomotorException:
-                gui.pos = 'unreachable'
+                gui.pos = self._motor.position()
+                gui.status = self._motor.status().as_dict()
+            except Exception as e:
+                gui.pos = str(e)
+
 
         self.gui = Gui(
-             [  'Pos:'  , 'pos'       , _ ],
-             [ ['Move by'] , '__nsteps__', 'steps' ]
+             [  'Pos:'     , 'pos'       , _       ],
+             [ ['Move to'] , '__nstepsto__', 'steps' ],
+             [ ['Move by'] , '__nstepsby__', 'steps' ],
+             [ 'Status:'   , 'status'    , _       ]
         )
-        self.gui.Move = moveby
-        self.gui.timer_start(getpos, 0.1)
+        self.gui.Moveby = moveby
+        self.gui.Moveto = moveto
+        self.gui.timer_start(getstatus, 0.1)
 
     def run(self, argv):
+        self._setUp(argv)
         self.gui.run()
 
     def terminate(self, signal, frame):
